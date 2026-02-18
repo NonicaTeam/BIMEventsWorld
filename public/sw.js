a927e657718f470af593ef869b9677f9e370dbdc
NonicaTeam <hello@nonica.io>
2026-02-18 17:58:11 +0100

    improvements web, add to home

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+// Minimal service worker for PWA install prompt.
+// Caches the app shell for offline support.
+const CACHE = 'bim-events-v1';
+const PRECACHE = ['/', '/en/'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
